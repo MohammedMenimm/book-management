f3dc6678895af0b87d9aa0941da39375dbe787e7
MohammedMenimm <Mohammed.Meniim@gmail.com>
2024-09-21 16:46:22 +0200

    Added Genre class

---
+++ src/main/java/com/example/bookmanagement/entities/Genre.java
@@ -0,0 +1,48 @@
+package com.example.bookmanagement.entities;
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "genres")
+    private List<Book> books;
+
+    // Constructors, getters, setters
+    public Genre() {
+    }
+
+    public Genre(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}

633905ebb73ee84e3a1997f8900e071ebd8936af
MohammedMenimm <Mohammed.Meniim@gmail.com>
2024-09-21 23:34:38 +0200

    Updated BookRepository

--- src/main/java/com/example/bookmanagement/repositories/BookRepository.java
+++ src/main/java/com/example/bookmanagement/repositories/BookRepository.java
@@ -2,6 +2,8 @@ package com.example.bookmanagement.repositories;
 
 import com.example.bookmanagement.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
